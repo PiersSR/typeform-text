f6aebf3b59500ae2a04e06442f5e0180e82c0493
Guy Mac <hello@guymac.eu>
2018-10-20 02:04:50 +0200

    Initial Slim boilerplate

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+// load classes
+
+require __DIR__ . '/vendor/autoload.php';
+spl_autoload_register(function ($class) {
+    include __DIR__ . '/src/' . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+});
+
+// setup Slim
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$app = new Slim\App(['settings' => $config]);
+
+session_start();
+
+$container = $app->getContainer();
+
+// Twig
+
+$container['view'] = function ($container) {
+    $templates = __DIR__ . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
+    $debug = false;
+    $debug = true;
+    $view = new Slim\Views\Twig($templates, compact('cache', 'debug'));
+    $view->getEnvironment()->addGlobal('_get', $_GET);
+
+    if ($debug) {
+        $view->addExtension(new \Slim\Views\TwigExtension(
+            $container['router'],
+            $container['request']->getUri()
+        ));
+        $view->addExtension(new \Twig_Extension_Debug());
+    }
+    return $view;
+};
+
+// 404
+
+$container['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c->view->render($response, '404.html.twig')->withStatus(404);
+    };
+};
+// Routes
+
+$app->get('/', function (Request $request, Response $response) {
+    
+});
