d7123e2bbdde13483cdaef3f31b0e2fd67e2f6a8
Guy Mac <hello@guymac.eu>
2018-10-20 14:13:09 +0200

    Add PDO to container

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ spl_autoload_register(function ($class) {
     include __DIR__ . '/src/' . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
 });
 
-
 // setup Slim
 
 $config['displayErrorDetails'] = true;
@@ -32,6 +31,17 @@ $container['twilio'] = function ($c) {
         $c['keys']['twilio']['sid'], 
         $c['keys']['twilio']['authToken']
     );
+};
+
+// Database
+
+$container['db'] = function ($c) {
+    $db = $c['keys']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'] . ';charset=utf8',
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
 };
 
 // Twig
@@ -112,6 +122,7 @@ function sendText($client) {
             'body' => '',
         ]   
     );
+}
 
 function postData($url, $data) {
     $options = array(
